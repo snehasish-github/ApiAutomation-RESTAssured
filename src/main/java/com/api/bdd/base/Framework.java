5ed623fed602daab3779319061fd8acaf73a56e1
Snehasish <karak.snehasish@gmail.com>
2020-07-25 23:11:42 +0800

    added extent report

---
+++ src/main/java/com/api/bdd/base/Framework.java
@@ -0,0 +1,26 @@
+package com.api.bdd.base;
+
+//import com.api.bdd.utils.ApiUtils;
+
+public class Framework {
+//    public static Framework framework;
+//    public ApiUtils apiutils;
+//
+//    private Framework() {
+//
+//    }
+//
+//    public static void crateInstance() {
+//        try {
+//            if (framework == null) {
+//                framework = new Framework();
+//                framework.apiutils = new ApiUtils();
+//                System.out.println("TestCount");
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+}
