0d9f5488ec3a9ef371ca78254ee149753912c4e3
Snehasish <karak.snehasish@gmail.com>
2023-07-29 23:11:33 +0530

    updated api utils getDeleteResponse

--- src/test/java/com/api/bdd/utils/ApiUtils.java
+++ src/test/java/com/api/bdd/utils/ApiUtils.java
@@ -102,6 +102,7 @@ public class ApiUtils {
         }
         catch(Exception e){
             e.printStackTrace();
+            System.out.println(e.getMessage());
         }
         return response;
     }
